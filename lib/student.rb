b67043de3ebee1a7b6a1692d7223fd301cef90c9
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-02-05 13:15:12 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,13 @@
-class Student 
+class Student < User
+
+  attr_reader :knowledge
+
+  def initialize
+    @knowledge = []
+  end
+
+  def learn(knowledge_learned)
+    @knowledge << knowledge_learned
+  end
 
 end
